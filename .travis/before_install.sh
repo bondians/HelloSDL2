914c60c155beca19fafd71635a987d2d20b77930
James Cook <mokus@deepbondi.net>
2015-12-19 16:36:31 -0800

    too. many. distractions.

--- .travis/before_install.sh
+++ .travis/before_install.sh
@@ -3,16 +3,16 @@
 set -e
 cd "$(dirname "$0")"
 
-case uname in
-    Darwin))
+case $(uname) in
+    Darwin)
         brew update
         brew install sdl2 sdl2_image
         ;;
-    Linux))
+    Linux)
         sudo apt-get update -qq
         sudo apt-get install -qq libsdl2-dev libsdl2-image-dev
         ;;
-    *))
+    *)
         echo "Unknown OS: $(uname)"
         exit 1
         ;;
